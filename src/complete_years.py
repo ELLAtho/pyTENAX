0c919dfe312fb1d40b5d7b1085eff36b47a23f3a
elrthomas <ellarthomas@outlook.com>
2024-11-01 18:57:41 +0000

    made loop to run trhough germany and produce array with info

--- src/complete_years.py
+++ src/complete_years.py
@@ -27,32 +27,88 @@ import sys
 
 from pyTENAX.intense import *
 import glob
-
+import datetime as dt
 
 
 S = TENAX(
         return_period = [2,5,10,20,50,100, 200],
         durations = [10, 60, 180, 360, 720, 1440],
         left_censoring = [0, 0.90],
+        tolerance=0.1,
     )
 
-
+start_time = time.time()
 files = glob.glob('Germany/*') #change Germany to folder name if looking through a different folder
-
+name_col = "ppt"
 
 data_meta = readIntense(files[0], only_metadata=True, opened=False)
 
 perc = data_meta.percent_missing_data
 yrs_no = data_meta.number_of_records/24/365
-
-# data=pd.read_parquet(file_path_input)
-# # Convert 'prec_time' column to datetime, if it's not already
-# data['prec_time'] = pd.to_datetime(data['prec_time'])
-# # Set 'prec_time' as the index
-# data.set_index('prec_time', inplace=True)
-# name_col = "prec_values" #name of column containing data to extract
+G = np.genfromtxt(files[0],skip_header=21)
 
 
+#extract start and end dates from metadata
+start_date_G= dt.datetime.strptime(data_meta.start_datetime, "%Y%m%d%H")
+end_date_G= dt.datetime.strptime(data_meta.end_datetime, "%Y%m%d%H")
+
+time_list_G= [start_date_G+ dt.timedelta(hours=x) for x in range(0, G.size)] #make timelist of size of FI
+# replace -999 with nan
+G[G == -999] = np.nan
+
+df = pd.DataFrame({'prec_time':time_list_G,name_col:G}).set_index('prec_time')
+
+data_clean = S.remove_incomplete_years(df, name_col) #remove incomplete years (below tolerance)
+full_yrs_no = np.size(data_clean)/24/365
+
+print('time for one file',str(time.time()-start_time))
+
+info = np.zeros([4,np.size(files)]) # filename, perc, years, cleaned_yrs
+start_time = time.time()
+
+i=0
+while i<np.size(files):
+    
+    data_meta = readIntense(files[i], only_metadata=True, opened=False)
+    info[0,i] = files[i][11:16] #filename
+    info[1,i] = data_meta.percent_missing_data #percent missing
+    info[2,i] = data_meta.number_of_records/24/365 #years uncleaned
+    
+    
+    if data_meta.number_of_records>365:
+        G = np.genfromtxt(files[i],skip_header=21)
+                   
+        #extract start and end dates from metadata
+        start_date_G= dt.datetime.strptime(data_meta.start_datetime, "%Y%m%d%H")
+        end_date_G= dt.datetime.strptime(data_meta.end_datetime, "%Y%m%d%H")
+    
+        time_list_G= [start_date_G+ dt.timedelta(hours=x) for x in range(0, G.size)] #make timelist of size of FI
+        # replace -999 with nan
+        G[G == -999] = np.nan
+        
+        df = pd.DataFrame({'prec_time':time_list_G,name_col:G}).set_index('prec_time')
+    
+        data_clean = S.remove_incomplete_years(df, name_col) #remove incomplete years (below tolerance)
+        info[3,i] = np.size(np.unique(data_clean.index.year))
+    else:
+        print('tiny!')
+        print(i)
+        print(str(time.time()-start_time))
+
+    i=i+1
+    
+    
+    
+print('time to loop thorugh germany',str(time.time()-start_time))
+np.save('germany_data.npy',info)
 
 
-data_clean = S.remove_incomplete_years(data, name_col)
+#filename = DE_+info[0,:]+.txt
+    
+    
+    
+    
+    
+    
+    
+    
