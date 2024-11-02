356c469bfb5e73e5ff222443430a20c87f0ff2d6
elrthomas <ellarthomas@outlook.com>
2024-11-02 08:38:26 +0000

    loop more generic, runs through US

--- src/complete_years.py
+++ src/complete_years.py
@@ -6,10 +6,6 @@ Created on Thu Oct 31 09:19:29 2024
 """
 
 
-## NEED TO RUN THE IMPORTS IN /SRC FOLDER THEN GO INTO DRIVE FOLDER
-
-import os
-# os.environ['USE_PYGEOS'] = '0'
 from os.path import dirname, abspath, join
 from os import getcwd
 import sys
@@ -19,16 +15,16 @@ CODE_DIR = join(THIS_DIR, 'src')
 RES_DIR =  join(THIS_DIR, 'res')
 sys.path.append(CODE_DIR)
 sys.path.append(RES_DIR)
+sys.path.append('D:')
 import numpy as np
 import pandas as pd
 from pyTENAX.pyTENAX import *
 import time 
-import sys
 
 from pyTENAX.intense import *
 import glob
 import datetime as dt
-
+import matplotlib.pyplot as plt
 
 S = TENAX(
         return_period = [2,5,10,20,50,100, 200],
@@ -37,45 +33,49 @@ S = TENAX(
         tolerance=0.1,
     )
 
-start_time = time.time()
-files = glob.glob('Germany/*') #change Germany to folder name if looking through a different folder
+
+country = 'US' #folder name that contains country data
 name_col = "ppt"
-
-data_meta = readIntense(files[0], only_metadata=True, opened=False)
-
-perc = data_meta.percent_missing_data
-yrs_no = data_meta.number_of_records/24/365
-G = np.genfromtxt(files[0],skip_header=21)
+year_size_limit = 3 # won't bother getting clean years if file smaller than this times percent missing
 
 
-#extract start and end dates from metadata
-start_date_G= dt.datetime.strptime(data_meta.start_datetime, "%Y%m%d%H")
-end_date_G= dt.datetime.strptime(data_meta.end_datetime, "%Y%m%d%H")
+files = glob.glob('D:/'+country+'/*') #list of filenames in folder
+str_start = 7+len(country) # for getting filename number in loop
 
-time_list_G= [start_date_G+ dt.timedelta(hours=x) for x in range(0, G.size)] #make timelist of size of FI
-# replace -999 with nan
-G[G == -999] = np.nan
-
-df = pd.DataFrame({'prec_time':time_list_G,name_col:G}).set_index('prec_time')
-
-data_clean = S.remove_incomplete_years(df, name_col) #remove incomplete years (below tolerance)
-full_yrs_no = np.size(data_clean)/24/365
-
-print('time for one file',str(time.time()-start_time))
-
+# initialize info table IT WOULD PROBABLY BE BETTER TO DO THIS AS A DF
 info = np.zeros([4,np.size(files)]) # filename, perc, years, cleaned_yrs
+times = np.zeros([2,np.size(files)]) # times table to check whats slow.
+latslons = np.zeros([2,np.size(files)]) #get location data of stations
+
 start_time = time.time()
 
-i=0
+
+print('folder size: '+str(np.size(files))+' files')
+print('first file: '+files[0])
+
+#quick bit
+i=0 
 while i<np.size(files):
-    
     data_meta = readIntense(files[i], only_metadata=True, opened=False)
-    info[0,i] = files[i][11:16] #filename
+    info[0,i] = files[i][str_start:str_start+6] #filename number only, 6 digits
     info[1,i] = data_meta.percent_missing_data #percent missing
     info[2,i] = data_meta.number_of_records/24/365 #years uncleaned
+    latslons[0,i] = data_meta.latitude
+    latslons[1,i] = data_meta.longitude
     
+    if i % 100 == 0:
+        print(i)
+    i=i+1
     
-    if data_meta.number_of_records>365:
+print('time to do quick: '+str(time.time()-start_time))
+    
+start_time = time.time()
+  
+    
+i=0 
+while i<np.size(files):  
+    data_meta = readIntense(files[i], only_metadata=True, opened=False) 
+    if data_meta.number_of_records*(1-data_meta.percent_missing_data/100)>365*24*year_size_limit: #don't bother reading ones less than x years incl missing data
         G = np.genfromtxt(files[i],skip_header=21)
                    
         #extract start and end dates from metadata
@@ -90,25 +90,38 @@ while i<np.size(files):
     
         data_clean = S.remove_incomplete_years(df, name_col) #remove incomplete years (below tolerance)
         info[3,i] = np.size(np.unique(data_clean.index.year))
+        times[1,i] = start_timei-time.time()
+        
     else:
-        print('tiny!')
+        print('less than '+str(year_size_limit)+' year')
         print(i)
         print(str(time.time()-start_time))
-
+        
+    if i % 100 == 0:
+        print(i)
     i=i+1
+
+  
     
-    
-    
-print('time to loop thorugh germany',str(time.time()-start_time))
-np.save('germany_data.npy',info)
+print('time to loop through '+country,str(time.time()-start_time))
+np.save('D:/'+country+'_data.npy',info)
 
 
+
+plt.hist(info[3])
+plt.title('Number of complete years '+country)
+plt.show()
+
+# num_above_10 = np.size(info[3][info[3]>10])
 #filename = DE_+info[0,:]+.txt
     
     
-    
-    
-    
-    
-    
+#info = np.load('D:/germany_data.npy')
+
+
+
+
+
+
+
     
