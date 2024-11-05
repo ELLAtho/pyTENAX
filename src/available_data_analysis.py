bef4ff8befe58f8bbb454daf1f03103d107771db
elrthomas <ellarthomas@outlook.com>
2024-11-05 14:08:38 +0000

    more messing with graphs

--- src/available_data_analysis.py
+++ src/available_data_analysis.py
@@ -66,7 +66,28 @@ while n<len(bounds):
     else:
         bounds[n] = 40
     n=n+1
+    
 
+cols = [0]*np.shape(dates_full)[1]
+n=0
+while n<len(cols):
+    if info_full[3][n]<10:
+        cols[n] = 'y'
+    elif info_full[3][n]<20:
+        cols[n] = 'g'
+    elif info_full[3][n]<30:
+        cols[n] = 'b'
+    else:
+        cols[n] = 'm'
+    n=n+1
+    
+        
+
+#BY DATASET
+fig = plt.figure(figsize=(10, 10))
+proj = ccrs.PlateCarree()
+ax1 = fig.add_subplot(1, 1, 1, projection=proj)
+ax1.coastlines()
 n=0
 while n<len(countries):
     plt.scatter(latslons[n][1],latslons[n][0],s=1.5,label = countries[n])
@@ -75,12 +96,87 @@ plt.legend()
 plt.show()
 
 
-
+#BY YEARS BINNED
+fig = plt.figure(figsize=(10, 10))
+proj = ccrs.PlateCarree()
+ax1 = fig.add_subplot(1, 1, 1, projection=proj)
+ax1.coastlines()
 sc = plt.scatter(latslons_full[1],latslons_full[0],s=1.5,c=bounds)
 plt.colorbar(sc)
 plt.show()
 
 
+#BY YEARS
+fig = plt.figure(figsize=(10, 10))
+proj = ccrs.PlateCarree()
+ax1 = fig.add_subplot(1, 1, 1, projection=proj)
+ax1.coastlines()
 sc = plt.scatter(latslons_full[1],latslons_full[0],s=1.5,c=info_full[3])
 plt.colorbar(sc,extend = 'max')
 plt.show()
+
+
+#EUROPE
+fig = plt.figure(figsize=(10, 10))
+proj = ccrs.PlateCarree()
+ax1 = fig.add_subplot(1, 1, 1, projection=proj)
+ax1.coastlines()
+sc = plt.scatter(latslons_full[1],latslons_full[0],s=1.5,c=cols)
+plt.xlim(-10,45)
+plt.ylim(20,75)
+
+plt.show()
+
+
+#JAPAN
+fig = plt.figure(figsize=(10, 10))
+proj = ccrs.PlateCarree()
+ax1 = fig.add_subplot(1, 1, 1, projection=proj)
+ax1.coastlines()
+sc = plt.scatter(latslons_full[1],latslons_full[0],s=1.5,c=cols)
+plt.xlim(110,150)
+plt.ylim(-10,48)
+plt.show()
+
+
+#ISLANDS
+fig = plt.figure(figsize=(10, 10))
+proj = ccrs.PlateCarree()
+ax1 = fig.add_subplot(1, 1, 1, projection=proj)
+ax1.coastlines()
+sc = plt.scatter(latslons_full[1],latslons_full[0],c=cols)
+plt.xlim(130,170)
+plt.ylim(-10,30)
+plt.show()
+
+
+
+#US
+fig = plt.figure(figsize=(10, 10))
+proj = ccrs.PlateCarree()
+ax1 = fig.add_subplot(1, 1, 1, projection=proj)
+
+ax1.coastlines()
+
+sc = plt.scatter(latslons_full[1],latslons_full[0],s=1.5,c=cols)
+plt.xlim(-140,-50)
+plt.ylim(20,75)
+plt.show()
+
+
+fig = plt.figure(figsize=(10, 10))
+proj = ccrs.PlateCarree()
+ax1 = fig.add_subplot(1, 1, 1, projection=proj)
+
+# Draw coastlines
+ax1.coastlines()
+sc = ax1.scatter(latslons_full[1],latslons_full[0],s=1.5,c=cols)
+ax1.set_xticks(np.arange(-180,190,20), crs=proj)
+ax1.set_yticks(np.arange(-90,100,20), crs=proj)
+plt.show()
+
+print('yellow: <10')
+print('green: 10-19')
+print('blue: 20-29')
+print('purple: 30+')
+
