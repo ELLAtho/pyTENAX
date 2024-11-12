3d6bc939e53cb29b8a3b374021ad4f84432519e0
ELLAtho <ella.thomas@unipd.it>
2024-11-12 17:56:27 +0100

    changed to Japan and deleted the nans bit because it didnt work properly

--- src/making_temp_times.py
+++ src/making_temp_times.py
@@ -28,8 +28,8 @@ import xarray as xr
 import time
 
 
-country = 'Germany'
-code_str = 'DE_' #string from beginning of file names
+country = 'Japan'
+code_str = 'JP_' #string from beginning of file names
 name_col = 'ppt'
 temp_name_col = "t2m"
 min_yrs = 0 #get temperature data for records > 0 years
@@ -44,12 +44,12 @@ info = info[info['cleaned_years']>=min_yrs] #filter out short data
 
 #READ IN ERA5 DATA
 T_files = sorted(glob.glob('D:/ERA5_land/'+country+'*/*'))
-edit_files = []
+nan_files = []
 
 #need to add in start and end dates here
 start_time = time.time()
 
-for i in np.arange(184,len(info)): #for each selected station
+for i in np.arange(0,len(info)): #for each selected station
     
     T_temp = [0]*np.size(T_files)
     for n, file in enumerate(T_files):
@@ -63,86 +63,8 @@ for i in np.arange(184,len(info)): #for each selected station
     #TODO: make this better... right now it doesn't cover full possibilities
     nan_perc = np.isnan(T_ERA).sum().item()/np.size(T_ERA)
     if nan_perc > T_nan_limit:
-        edit_files.append(info.station[info.index[i]])
-        print('percentage nans: '+str(100*nan_perc))
-        print(f'({info.latitude[info.index[i]]},{info.longitude[info.index[i]]})')
-        print(str(info.station[info.index[i]]))
-        j=1
-        while nan_perc > T_nan_limit:
-            T_temp = [0]*np.size(T_files)
-            new_lat = info.latitude[info.index[i]]+0.1*j
-            new_lon = info.longitude[info.index[i]]+0.1*(j-1)
-            for n, file in enumerate(T_files):
-                with xr.open_dataarray(file) as da:
-                    T_temp[n] = da.sel(latitude = new_lat,method = 'nearest').sel(longitude = new_lon,method = 'nearest')
-                
-            T_ERA = xr.concat(T_temp,dim = 'valid_time')  #combine multiple time files
-            nan_perc = np.isnan(T_ERA).sum().item()/np.size(T_ERA)
-            print(f"Percentage of NaNs after offset lat {+0.1 * j}: {100 * nan_perc:.2f}%")
-            if nan_perc < T_nan_limit:
-                break
-            
-            new_lat = info.latitude[info.index[i]]+0.1*(j-1)
-            new_lon = info.longitude[info.index[i]]+0.1*j
-            for n, file in enumerate(T_files):
-                with xr.open_dataarray(file) as da:
-                    T_temp[n] = da.sel(latitude = new_lat,method = 'nearest').sel(longitude = new_lon,method = 'nearest')
-                
-            T_ERA = xr.concat(T_temp,dim = 'valid_time')  #combine multiple time files
-            nan_perc = np.isnan(T_ERA).sum().item()/np.size(T_ERA)
-            print(f"Percentage of NaNs after offset lon {+0.1 * j}: {100 * nan_perc:.2f}%")
-            if nan_perc < T_nan_limit:
-                break
-                       
-            new_lat = info.latitude[info.index[i]]-0.1*j
-            new_lon = info.longitude[info.index[i]]-0.1*(j-1)
-            for n, file in enumerate(T_files):
-                with xr.open_dataarray(file) as da:
-                    T_temp[n] = da.sel(latitude = new_lat,method = 'nearest').sel(longitude = new_lon,method = 'nearest')
-                
-            T_ERA = xr.concat(T_temp,dim = 'valid_time')  #combine multiple time files
-            nan_perc = np.isnan(T_ERA).sum().item()/np.size(T_ERA)
-            print(f"Percentage of NaNs after offset lat {-0.1 * j}: {100 * nan_perc:.2f}%")
-            if nan_perc < T_nan_limit:
-                break
-            
-            new_lat = info.latitude[info.index[i]]-0.1*(j-1)
-            new_lon = info.longitude[info.index[i]]-0.1*j
-            for n, file in enumerate(T_files):
-                with xr.open_dataarray(file) as da:
-                    T_temp[n] = da.sel(latitude = new_lat,method = 'nearest').sel(longitude = new_lon,method = 'nearest')
-                
-            T_ERA = xr.concat(T_temp,dim = 'valid_time')  #combine multiple time files
-            nan_perc = np.isnan(T_ERA).sum().item()/np.size(T_ERA)
-            print(f"Percentage of NaNs after offset lon {-0.1 * j}: {100 * nan_perc:.2f}%")    
-            new_lat = info.latitude[info.index[i]]-0.1*j
-            new_lon = info.longitude[info.index[i]]-0.1*j
-            for n, file in enumerate(T_files):
-                with xr.open_dataarray(file) as da:
-                    T_temp[n] = da.sel(latitude = new_lat,method = 'nearest').sel(longitude = new_lon,method = 'nearest')
-                
-            T_ERA = xr.concat(T_temp,dim = 'valid_time')  #combine multiple time files
-            nan_perc = np.isnan(T_ERA).sum().item()/np.size(T_ERA)
-            print(f"Percentage of NaNs after offset double {-0.1 * j}: {100 * nan_perc:.2f}%")
-            
-            new_lat = info.latitude[info.index[i]]+0.1*j
-            new_lon = info.longitude[info.index[i]]+0.1*j
-            for n, file in enumerate(T_files):
-                with xr.open_dataarray(file) as da:
-                    T_temp[n] = da.sel(latitude = new_lat,method = 'nearest').sel(longitude = new_lon,method = 'nearest')
-                
-            T_ERA = xr.concat(T_temp,dim = 'valid_time')  #combine multiple time files
-            nan_perc = np.isnan(T_ERA).sum().item()/np.size(T_ERA)
-            print(f"Percentage of NaNs after offset double {0.1 * j}: {100 * nan_perc:.2f}%")    
-            
-            j=j+1
-            if j == 10:
-                print('too far!')
-                print(str(info.station[info.index[i]])+' is all nans')
-                break
-        print(str(info.latitude[info.index[i]])+' became '+str(new_lat))
-        print(str(info.longitude[info.index[i]])+' became '+str(new_lon))
-        print(T_ERA)
+        nan_files.append(info.station[info.index[i]])
+        
     
     
     
