9e258993559adfc62d11e5c6a70149323296b7bd
ELLAtho <ella.thomas@unipd.it>
2025-02-14 11:53:05 +0100

    plotted some graphs, saved the data from looping

--- src/goodness_of_fits_map.py
+++ src/goodness_of_fits_map.py
@@ -51,6 +51,17 @@ name_len = 5
 min_startdate = dt.datetime(1900,1,1) #this is for if havent read all ERA5 data yet
 censor_thr = 0.9
 
+
+# country = 'Japan'
+# ERA_country = 'Japan'
+# country_save = 'Japan'
+# code_str = 'JP_'
+# minlat,minlon,maxlat,maxlon = 24, 122.9, 45.6, 145.8 #JAPAN
+# name_len = 5
+# min_startdate = dt.datetime(1900,1,1) #this is for if havent read all ERA5 data yet
+# censor_thr = 0.9
+
+
 name_col = 'ppt' 
 temp_name_col = "t2m"
 min_yrs = 10 
@@ -134,113 +145,257 @@ else:
     pass
 
 
+save_name = f"{drive}:/outputs/{country_save}\\return_levels.csv"
+output_files = glob.glob(f"{drive}:/outputs/{country_save}/*")
 
-
-S = TENAX(
-        return_period = [1.1,1.2,1.5,2,5,10,20,50,100, 200],
-        durations = [60, 180, 360, 720, 1440],
-        left_censoring = [0, censor_thr],
-        alpha = alpha_set,
-        min_ev_dur = 60,
-        niter_smev = 1000, 
-    )
-
-RL = [0] * len(new_df)
-start_time = [0] * len(new_df)
-FRMSE = [0] * len(new_df)
-
-
-for i in np.arange(0, len(new_df)):
-    start_time[i] = time.time() 
-    #read in ppt data
-    file_name = f"{drive}:/{country}/{code_str}{df_parameters.station.iloc[i]}"
+if save_name not in output_files:
+    print("levels not calculated yet, doing now.")
+    S = TENAX(
+            return_period = [1.1,1.2,1.5,2,5,10,20,50,100, 200],
+            durations = [60, 180, 360, 720, 1440],
+            left_censoring = [0, censor_thr],
+            alpha = alpha_set,
+            min_ev_dur = 60,
+            niter_smev = 1000, 
+        )
     
-    if 'code_str' in locals():
-        G,data_meta = read_GSDR_file(f"{file_name}.txt",name_col)
-    else:
-        G = pd.read_csv(f"{file_name}.csv")
-        G['prec_time'] = pd.to_datetime(G['prec_time'])
-        G.set_index('prec_time', inplace=True)
+    RL = [0] * len(new_df)
+    RL_0 = [0] * len(new_df)
+    RL_5 = [0] * len(new_df)
+    AMS_sort = [0] * len(new_df)
+    start_time = [0] * len(new_df)
+    FRMSE = [0] * len(new_df)
+    FRMSE_5 = [0] * len(new_df)
+    FRMSE_0 = [0] * len(new_df)
+    
+    
+    for i in np.arange(0, len(new_df)):
+        start_time[i] = time.time() 
+        #read in ppt data
+        file_name = f"{drive}:/{country}/{code_str}{df_parameters.station.iloc[i]}"
         
-    print(G[0:3])
-    ######################################################################
-    #TENAX  AMS
-
-    data = G 
-    data = S.remove_incomplete_years(data, name_col)
+        if 'code_str' in locals():
+            G,data_meta = read_GSDR_file(f"{file_name}.txt",name_col)
+        else:
+            G = pd.read_csv(f"{file_name}.csv")
+            G['prec_time'] = pd.to_datetime(G['prec_time'])
+            G.set_index('prec_time', inplace=True)
+            
+        ######################################################################
+        #TENAX  AMS
     
-    df_arr = np.array(data[name_col])
-    df_dates = np.array(data.index)
-    
-    #extract indexes of ordinary events
-    #these are time-wise indexes =>returns list of np arrays with np.timeindex
-    idx_ordinary=S.get_ordinary_events(data=df_arr,dates=df_dates, name_col=name_col,  check_gaps=False)
+        data = G 
+        data = S.remove_incomplete_years(data, name_col)
         
+        df_arr = np.array(data[name_col])
+        df_dates = np.array(data.index)
+        
+        #extract indexes of ordinary events
+        #these are time-wise indexes =>returns list of np arrays with np.timeindex
+        idx_ordinary=S.get_ordinary_events(data=df_arr,dates=df_dates, name_col=name_col,  check_gaps=False)
+            
+        
+        #get ordinary events by removing too short events
+        #returns boolean array, dates of OE in TO, FROM format, and count of OE in each years
+        arr_vals,arr_dates,n_ordinary_per_year=S.remove_short(idx_ordinary)
+        
+        #assign ordinary events values by given durations, values are in depth per duration, NOT in intensity mm/h
+        dict_ordinary, dict_AMS = S.get_ordinary_events_values(data=df_arr,dates=df_dates, arr_dates_oe=arr_dates)
+        
+        AMS = dict_AMS['60']
+        
+        # Define the model parameters by reading in those already saved
+        g_phat = [df_parameters.mu.iloc[i],df_parameters.sigma.iloc[i]]
+        #free
+        F_phat = [new_df.kappa.iloc[i],new_df.b.iloc[i],
+                  new_df["lambda"].iloc[i],new_df.a.iloc[i]]
+        #5% sig
+        F_phat_5 = [df_parameters.kappa.iloc[i],df_parameters.b.iloc[i],
+                    df_parameters["lambda"].iloc[i],df_parameters.a.iloc[i]]
+        #b always 0
+        F_phat_0 = [df_parameters_0.kappa.iloc[i],df_parameters_0.b.iloc[i],
+                    df_parameters_0["lambda"].iloc[i],new_df.a.iloc[i]]
+        
+        n = df_parameters.n_events_per_yr.iloc[i]
+        
+        # Getting predicted return levels
+        AMS_sort[i] = AMS.sort_values(by=['AMS'])['AMS']
+        plot_pos = np.arange(1,np.size(AMS_sort)+1)/(1+np.size(AMS_sort))
+        
+        eRP = 1/(1-plot_pos)
+        S.return_period = eRP
+        
+        T_min = g_phat[0] - 2.5 * g_phat[1]
+        T_max = g_phat[0] + 2.5 * g_phat[1]
+        Ts = np.arange(T_min - S.temp_delta, T_max + S.temp_delta, S.temp_res_monte_carlo)
+        
+        RL[i], __, __ = S.model_inversion(F_phat, g_phat, n, Ts)
+        RL_5[i], __, __ = S.model_inversion(F_phat_5, g_phat, n, Ts)
+        RL_0[i], __, __ = S.model_inversion(F_phat_0, g_phat, n, Ts)
+        
+        diffs = RL[i] - AMS_sort
+        diffs_5 = RL_5[i] - AMS_sort
+        diffs_0 = RL_0[i] - AMS_sort
+        
+        FRMSE[i] = np.sqrt(np.sum(diffs**2)/len(diffs))/(np.sum(AMS_sort)/len(diffs))
+        FRMSE_5[i] = np.sqrt(np.sum(diffs_5**2)/len(diffs_5))/(np.sum(AMS_sort)/len(diffs_5))
+        FRMSE_0[i] = np.sqrt(np.sum(diffs_0**2)/len(diffs_0))/(np.sum(AMS_sort)/len(diffs_0))
+        
+        
+        print(f"Free {FRMSE[i]}, 5% sig {FRMSE_5[i]}, b always 0 {FRMSE_0[i]}")
+        time_taken = (time.time()-start_time[i-9])/10
+        time_left = (len(new_df)-i)*time_taken/60
+        print(f"{i}/{len(new_df)}. Approx time left: {time_left:.0f} mins") #this is only correct after 50 loops
     
-    #get ordinary events by removing too short events
-    #returns boolean array, dates of OE in TO, FROM format, and count of OE in each years
-    arr_vals,arr_dates,n_ordinary_per_year=S.remove_short(idx_ordinary)
     
-    #assign ordinary events values by given durations, values are in depth per duration, NOT in intensity mm/h
-    dict_ordinary, dict_AMS = S.get_ordinary_events_values(data=df_arr,dates=df_dates, arr_dates_oe=arr_dates)
+    AMS_sort_save = [AMS_sort[j].to_numpy() for j in np.arange(0,len(AMS_sort))]
+    RL_df = pd.DataFrame({'station': df_parameters.station, 'obs_AMS': AMS_sort_save, 'return_levels': RL, 'return_levels_5%': RL_5, 'return_levels_b0': RL_0})
+    RL_df.to_csv(f"{drive}:/outputs/{country_save}/return_levels.csv",index=False)
     
-    AMS = dict_AMS['60']
+    FRMSE_df = pd.DataFrame({'station': df_parameters.station,
+                             'FRMSE': FRMSE,
+                             'FRMSE_5': FRMSE_5,
+                             'FRMSE_0': FRMSE_0})
     
-    # Define the model parameters by reading in those already saved
-    g_phat = [df_parameters.mu.iloc[i],df_parameters.sigma.iloc[i]]
-    #free
-    F_phat = [new_df.kappa.iloc[i],new_df.b.iloc[i],
-              new_df["lambda"].iloc[i],new_df.a.iloc[i]]
-    #5% sig
-    F_phat_5 = [df_parameters.kappa.iloc[i],df_parameters.b.iloc[i],
-                df_parameters["lambda"].iloc[i],df_parameters.a.iloc[i]]
-    #b always 0
-    F_phat_0 = [df_parameters_0.kappa.iloc[i],df_parameters_0.b.iloc[i],
-                df_parameters_0["lambda"].iloc[i],new_df.a.iloc[i]]
-    
-    n = df_parameters.n_events_per_yr.iloc[i]
-    
-    # Getting predicted return levels
-    AMS_sort = AMS.sort_values(by=['AMS'])['AMS']
-    plot_pos = np.arange(1,np.size(AMS_sort)+1)/(1+np.size(AMS_sort))
-    
-    eRP = 1/(1-plot_pos)
-    S.return_period = eRP
-    
-    T_min = g_phat[0] - 2.5 * g_phat[1]
-    T_max = g_phat[0] + 2.5 * g_phat[1]
-    Ts = np.arange(T_min - S.temp_delta, T_max + S.temp_delta, S.temp_res_monte_carlo)
-    
-    RL[i], __, __ = S.model_inversion(F_phat, g_phat, n, Ts)
-    
-    diffs = RL[i] - AMS_sort
-    
-    FRMSE[i] = np.sqrt(np.sum(diffs**2)/len(diffs))/(np.sum(AMS_sort)/len(diffs))
-    #TODO: need to do the 5% and 0 ones still
-    
-    
-    
-    time_taken = (time.time()-start_time[i-9])/10
-    time_left = (len(new_df)-i)*time_taken/60
-    print(f"{i}/{len(new_df)}. Approx time left: {time_left:.0f} mins") #this is only correct after 50 loops
+    FRMSE_df.to_csv(f"{drive}:/outputs/{country_save}/FRMSE.csv",index=False)
+else:
+    print("Files already saved, reading")
+    RL_df = pd.read_csv(f"{drive}:/outputs/{country_save}/return_levels.csv", dtype={'station': str})
+    FRMSE_df = pd.read_csv(f"{drive}:/outputs/{country_save}/FRMSE.csv", dtype={'station': str})
+
+
+
+#maps
+lon_lims = [truncate_neg(np.min(df_parameters.longitude),2.5),np.ceil(np.max(df_parameters.longitude/2.5))*2.5]
+lat_lims = [truncate_neg(np.min(df_parameters.latitude),2.5),np.ceil(np.max(df_parameters.latitude/2.5))*2.5]
+s = 7
+cmap = 'magma_r'
+
+
+fig = plt.figure(figsize=(10, 10))
+norm = mcolors.Normalize(vmin=0, vmax=1)
+
+
+proj = ccrs.PlateCarree()
+ax1 = fig.add_subplot(2, 2, 1, projection=proj)
+
+# Add map features
+ax1.coastlines()
+ax1.add_feature(cfeature.BORDERS, linestyle=':')
+
+
+sc = ax1.scatter(
+    df_parameters.longitude,
+    df_parameters.latitude,
+    c=FRMSE_df.FRMSE,
+    cmap=cmap,
+    norm = norm,
+    s = s,
+)
+ax1.set_xticks(np.arange(lon_lims[0],lon_lims[1]+1,2.5), crs=proj)
+ax1.set_yticks(np.arange(lat_lims[0],lat_lims[1]+1,2.5), crs=proj)
+ax1.tick_params(labelsize=12)  
+
+plt.xlim(lon_lims[0]-1,lon_lims[1]+1)
+plt.ylim(lat_lims[0]-1,lat_lims[1]+1)
+ax1.set_title("b free")
+
+
+
+ax2 = fig.add_subplot(2, 2, 2, projection=proj)
+ax2.coastlines()
+ax2.add_feature(cfeature.BORDERS, linestyle=':')
+
+
+sc = ax2.scatter(
+    df_parameters.longitude,
+    df_parameters.latitude,
+    c=FRMSE_df.FRMSE_5,
+    cmap=cmap,
+    norm = norm,  
+    s = s,
+)
+ax2.set_xticks(np.arange(lon_lims[0],lon_lims[1]+1,2.5), crs=proj)
+ax2.set_yticks(np.arange(lat_lims[0],lat_lims[1]+1,2.5), crs=proj)
+ax2.tick_params(labelsize=12)  
+plt.xlim(lon_lims[0]-1,lon_lims[1]+1)
+plt.ylim(lat_lims[0]-1,lat_lims[1]+1)
+ax2.set_title("b significant at 5% level")
+
+
+ax3 = fig.add_subplot(2, 2, 3, projection=proj)
+ax3.coastlines()
+ax3.add_feature(cfeature.BORDERS, linestyle=':')
+
+
+sc = ax3.scatter(
+    df_parameters.longitude,
+    df_parameters.latitude,
+    c=FRMSE_df.FRMSE_0,
+    cmap=cmap,
+    norm = norm,  
+    s = s,  
+)
+ax3.set_xticks(np.arange(lon_lims[0],lon_lims[1]+1,2.5), crs=proj)
+ax3.set_yticks(np.arange(lat_lims[0],lat_lims[1]+1,2.5), crs=proj)
+ax3.tick_params(labelsize=12)  
+plt.xlim(lon_lims[0]-1,lon_lims[1]+1)
+plt.ylim(lat_lims[0]-1,lat_lims[1]+1)
+ax3.set_title("b = 0")
+
+ax4 = fig.add_subplot(2, 2, 4, projection=proj)
+ax4.coastlines()
+ax4.add_feature(cfeature.BORDERS, linestyle=':')
+
+
+sc4 = ax4.scatter(
+    val_info.longitude,
+    val_info.latitude,
+    c=val_info.cleaned_years,
+    cmap="viridis",
+    s = s,  
+)
+ax4.set_xticks(np.arange(lon_lims[0],lon_lims[1]+1,2.5), crs=proj)
+ax4.set_yticks(np.arange(lat_lims[0],lat_lims[1]+1,2.5), crs=proj)
+ax4.tick_params(labelsize=12)  
+plt.xlim(lon_lims[0]-1,lon_lims[1]+1)
+plt.ylim(lat_lims[0]-1,lat_lims[1]+1)
+
+fig.subplots_adjust(right=0.85)
+
+cbar_ax4 = fig.add_axes([0.87, 0.12, 0.03, 0.32])  # Position for the colorbar outside
+cb4 = plt.colorbar(sc4, cax=cbar_ax4)  # Colorbar for ax4
+cb4.set_label('Number of complete years', fontsize=14)
+cb4.ax.tick_params(labelsize=12)
+
+ax4.set_title("cleaned years")
 
 
 
 
-#TODO: need to save the return levels and the FRMSE and plot
+
+# Add a colorbar at the bottom
+cbar_ax = fig.add_subplot([0.15, 0.02, 0.7, 0.03])  # Position for the colorbar
+cb = plt.colorbar(sc, cax=cbar_ax, orientation='horizontal')
+cb.set_label('FRMSE', fontsize=14)
+cb.ax.tick_params(labelsize=12)
+
+# Set x and y ticks
+
+
+#fig.tight_layout()
+fig.suptitle(f'GSDR: {ERA_country}. FRMSE on RL', fontsize=16)
+plt.show()
 
 
 
+# CHECKS
+j = 0
+plot_pos = np.arange(1,np.size(RL_df.obs_AMS.iloc[j])+1)/(1+np.size(RL_df.obs_AMS.iloc[j]))
 
+eRP = 1/(1-plot_pos)
 
+TNX_FIG_valid(RL_df.obs_AMS.iloc[j],eRP,RL_df.return_levels_5)
 
-
-
-
-
-
-
-
-
-
+plt.show()
 
