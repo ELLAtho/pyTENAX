0eb23a75a7861205d092f2e3eecc926b6f72f684
ELLAtho <ella.thomas@unipd.it>
2025-03-05 15:15:53 +0100

    getting mean within a radius

---
+++ src/parameter_space_mean.py
@@ -0,0 +1,217 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Mar  5 10:56:48 2025
+
+@author: ellar
+"""
+
+from os.path import dirname, join
+from os import getcwd
+import sys
+#run this fro src folder, otherwise it doesn't work
+THIS_DIR = dirname(getcwd())
+CODE_DIR = join(THIS_DIR, 'src')
+RES_DIR =  join(THIS_DIR, 'res')
+sys.path.append(CODE_DIR)
+sys.path.append(RES_DIR) 
+sys.path.append('D:')
+import numpy as np
+import pandas as pd
+from scipy.stats import gaussian_kde
+
+import datetime as dt
+import glob
+
+from pyTENAX.intense import *
+from pyTENAX.pyTENAX import *
+from pyTENAX.globalTENAX import *
+
+import xarray as xr
+import time
+import matplotlib.pyplot as plt
+import matplotlib.colors as mcolors
+import cartopy.crs as ccrs
+import cartopy.feature as cfeature
+from matplotlib.colors import LinearSegmentedColormap
+import matplotlib.patches as patches
+from scipy.stats import kendalltau, pearsonr, spearmanr
+from scipy.interpolate import interp1d
+from matplotlib import cm
+
+
+
+drive = 'D'
+alpha_set = 0
+
+radius = 150 #radius in km
+
+
+
+# country = 'Germany' 
+# ERA_country = 'Germany'
+# country_save = 'Germany'
+# code_str = 'DE_'
+# minlat,minlon,maxlat,maxlon = 47, 3, 55, 15 #GERMANY
+# name_len = 5
+# min_startdate = dt.datetime(1900,1,1) #this is for if havent read all ERA5 data yet
+# censor_thr = 0.9
+
+
+country = 'Japan'
+ERA_country = 'Japan'
+country_save = 'Japan'
+code_str = 'JP_'
+minlat,minlon,maxlat,maxlon = 24, 122.9, 45.6, 145.8 #JAPAN
+name_len = 5
+min_startdate = dt.datetime(1900,1,1) #this is for if havent read all ERA5 data yet
+censor_thr = 0.9
+
+# country = 'US' 
+# ERA_country = 'US'
+# country_save = 'US_main'
+# code_str = 'US_'
+# minlat,minlon,maxlat,maxlon = 24, -125, 56, -66  
+# name_len = 6
+# min_startdate = dt.datetime(1950,1,1) #this is for if havent read all ERA5 data yet
+# censor_thr = 0.9
+
+
+
+name_col = 'ppt' 
+temp_name_col = "t2m"
+min_yrs = 10 
+
+#READ IN META INFO FOR COUNTRY
+info = pd.read_csv(drive+':/metadata/'+country+'_fulldata.csv', dtype={'station': str})
+
+
+# shouldn't need this anymore as changed files
+# if name_len!=0:
+#     info.station = info['station'].apply(lambda x: f'{int(x):0{name_len}}') #need to edit this according to file
+# else:
+#     pass
+
+info.startdate = pd.to_datetime(info.startdate)
+info.enddate = pd.to_datetime(info.enddate)
+
+#select stations
+
+
+val_info = info[info['cleaned_years']>=min_yrs] #filter out stations that are less than min
+
+S = TENAX(
+        return_period = [1.1,1.2,1.5,2,5,10,20,50,100, 200],
+        durations = [60, 180, 360, 720, 1440],
+        left_censoring = [0, censor_thr],
+        alpha = alpha_set,
+        min_ev_dur = 60,
+        niter_smev = 1000, 
+    )
+
+#getting info of correct size
+if 'min_startdate' in locals():    
+    val_info = val_info[val_info['startdate']>=min_startdate]
+else:
+    pass
+
+if 'minlat' in locals():
+    
+    val_info = val_info[val_info['latitude']>=minlat] #filter station locations to within ERA bounds
+    val_info = val_info[val_info['latitude']<=maxlat]
+    val_info = val_info[val_info['longitude']>=minlon]
+    val_info = val_info[val_info['longitude']<=maxlon]
+    
+else:
+    pass
+
+
+
+files = glob.glob(drive+':/'+country+'/*') #list of files in country folder
+files_sel = [files[i] for i in val_info.index]
+
+
+## READ IN FILES
+save_path_neg = drive + ':/outputs/'+country_save+'\\parameters_neg.csv'
+df_savename = drive + ':/outputs/'+country_save+'\\parameters.csv'
+
+
+df_parameters = pd.read_csv(df_savename, dtype={'station': str}) 
+TENAX_use = pd.read_csv(drive + ':/outputs/'+country_save+'/TENAX_parameters.csv') #save calculated parameters
+
+df_parameters_0 = pd.read_csv(f"{drive}:/outputs/{country_save}_b0/parameters.csv", dtype={'station': str})
+
+# for some reason in germany there is one less row...
+    
+
+
+
+if np.size(glob.glob(save_path_neg)) != 0:
+    df_parameters_neg = pd.read_csv(save_path_neg, dtype={'station': str})
+
+    #dataframe with all values
+    new_df = df_parameters[['station','latitude','longitude','b','kappa','lambda','a']].copy()
+    
+    mask = new_df['b'] == 0
+    
+    new_df.loc[mask, 'b'] = df_parameters_neg['b2'].to_numpy()
+    new_df.loc[mask, 'kappa'] = df_parameters_neg['kappa2'].to_numpy()
+    new_df.loc[mask, 'lambda'] = df_parameters_neg['lambda2'].to_numpy()
+    new_df.loc[mask, 'a'] = df_parameters_neg['a2'].to_numpy()
+
+else:
+    new_df = df_parameters.copy()
+
+#merging the dataframes to ensure station consistency
+missing_rows = pd.merge(df_parameters.station, df_parameters_0.station, how='left', indicator=True).query('_merge == "left_only"').drop('_merge', axis=1)
+if len(missing_rows) != 0:
+    print("miss-match, dropping")
+    df_parameters = df_parameters.drop(missing_rows.index)
+    new_df = new_df.drop(missing_rows.index)
+else:
+    pass
+
+
+
+#get matrix of the distances between each station
+distance_savename = f"{drive}:/outputs/{country_save}\\distances_matrix.npy"
+
+if distance_savename not in glob.glob(f"{drive}:/outputs/{country_save}\\*"):
+    print("calculating distances")          
+    distances_matrix = np.zeros((len(new_df.latitude),len(new_df.latitude)))
+    
+    for i in range(len(new_df.latitude)):
+        for j in range(len(new_df.latitude)):
+            distances_matrix[i,j] = calculate_distance(new_df.latitude.iloc[i], 
+                                                       new_df.longitude.iloc[i], 
+                                                       new_df.latitude.iloc[j], 
+                                                       new_df.longitude.iloc[j])
+    
+    np.save(f"{drive}:/outputs/{country_save}\\distances_matrix", distances_matrix)
+else:
+    distances_matrix = np.load(distance_savename)
+
+
+#calculate b as average
+b2 = [0]*len(new_df.latitude)
+n_stations_in_group = [0]*len(new_df.latitude)
+
+for i in range(len(new_df.latitude)):
+    if pd.isna(new_df.b.iloc[i]):
+        b2[i] = np.nan
+        n_stations_in_group[i] = np.nan
+    else:
+        station_distances = distances_matrix[i,:]
+        close_locs = np.where(station_distances<=radius*1000)
+        
+        b2[i] = np.mean(new_df.b.iloc[close_locs])
+        n_stations_in_group[i] = len(close_locs[0])
+        
+plt.hist(n_stations_in_group)
+plt.xlabel("number of stations within radius")
+plt.title(f"{country_save}. radius = {radius}")
+
+
+
+
+
+
