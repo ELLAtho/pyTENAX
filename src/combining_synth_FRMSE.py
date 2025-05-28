07a0867edc0ec0851e637beb20a86a5c872ad19d
ELLAtho <ella.thomas@unipd.it>
2025-05-28 17:08:23 +0200

    ssome nice graphs in the FRMSE combined

--- src/combining_synth_FRMSE.py
+++ src/combining_synth_FRMSE.py
@@ -58,21 +58,61 @@ for i in range(len(uses)):
     
     Ts = np.arange(g_phat_typical[0]-2*g_phat_typical[1] - S.temp_delta, g_phat_typical[0]+2*g_phat_typical[1] + S.temp_delta, S.temp_res_monte_carlo)
     
-    RL_typical, _, _ = S.model_inversion(F_phat_typical, g_phat_typical, uses[i].n, Ts)
+    RL_typical, _, _ = S.model_inversion(F_phat_typical, g_phat_typical, uses[i].n, Ts,b_exp = i%2)
     RL_true.append(RL_typical)
 
 
 
+#for now only the first one because other one not ready yet but should be a loop
+
+# diff_free = synth_RL[0].free_10 - RL_true[0][0]
+# diff_0 = synth_RL[0].b0_10 - RL_true[0][0]
+# diff_set = synth_RL[0].set_10 - RL_true[0][0]
+
+# FRMSE_10 = pd.DataFrame({
+#     "free" : np.sqrt(np.sum(diff_free**2))/RL_true[0][0],
+#     "b0" : np.sqrt(np.sum(diff_0**2))/RL_true[0][0],
+#     "set" : np.sqrt(np.sum(diff_set**2))/RL_true[0][0]
+#     })
+
+
+#violins of free
+plt.violinplot([synth_RL[0].free_10,synth_RL[0].free_20,synth_RL[0].free_50,synth_RL[0].free_100],widths = [10*0.08,20*0.1,50*0.08,100*0.08], positions = [10*0.8,20*0.8,50*0.8,100*0.8],showmeans = True)
+#violins of set
+plt.violinplot([synth_RL[0].set_10,synth_RL[0].set_20,synth_RL[0].set_50,synth_RL[0].set_100],widths = [10*0.1,20*0.1,50*0.1,100*0.1],positions = [10,20,50,100],showmeans = True)
+#violins of 0
+plt.violinplot([synth_RL[0].b0_10,synth_RL[0].b0_20,synth_RL[0].b0_50,synth_RL[0].b0_100],widths = [10*0.1/0.8,20/0.8*0.1,50/0.8*0.1,100/0.8*0.1],positions = [10/0.8,20/0.8,50/0.8,100/0.8],showmeans = True)
+
+plt.plot([10*0.8,10,10/0.8],[RL_true[0][0]]*3,color = "r")
+plt.plot([20*0.8,20,20/0.8],[RL_true[0][1]]*3,color = "r")
+plt.plot([50*0.8,50,50/0.8],[RL_true[0][2]]*3,color = "r")
+plt.plot([100*0.8,100,100/0.8],[RL_true[0][3]]*3,color = "r")
+
+
+
+plt.xscale("log")
+plt.title("linear b")
+plt.ylim(20,120)
+plt.show()
 
 
 
 
+#violins of free
+plt.violinplot([synth_RL[0].free_10_exp,synth_RL[0].free_20_exp,synth_RL[0].free_50_exp,synth_RL[0].free_100_exp],widths = [10*0.08,20*0.1,50*0.08,100*0.08], positions = [10*0.8,20*0.8,50*0.8,100*0.8],showmeans = True)
+#violins of set
+plt.violinplot([synth_RL[0].set_10_exp,synth_RL[0].set_20_exp,synth_RL[0].set_50_exp,synth_RL[0].set_100_exp],widths = [10*0.1,20*0.1,50*0.1,100*0.1],positions = [10,20,50,100],showmeans = True)
+#violins of 0
+plt.violinplot([synth_RL[0].b0_10_exp,synth_RL[0].b0_20_exp,synth_RL[0].b0_50_exp,synth_RL[0].b0_100_exp],widths = [10*0.1/0.8,20/0.8*0.1,50/0.8*0.1,100/0.8*0.1],positions = [10/0.8,20/0.8,50/0.8,100/0.8],showmeans = True)
+plt.plot([10*0.8,10,10/0.8],[RL_true[1][0]]*3,color = "r")
+plt.plot([20*0.8,20,20/0.8],[RL_true[1][1]]*3,color = "r")
+plt.plot([50*0.8,50,50/0.8],[RL_true[1][2]]*3,color = "r")
+plt.plot([100*0.8,100,100/0.8],[RL_true[1][3]]*3,color = "r")
 
-
-
-
-
-
+plt.xscale("log")
+plt.title("exponential b")
+plt.ylim(20,120)
+plt.show()
 
 
 
